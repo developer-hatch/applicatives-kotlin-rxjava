92ecd25f8a81e66e62fdc3c9795f587f5d3bb514
Damian Lattenero <damianlattenero@gmail.com>
2023-05-06 11:53:09 -0300

    Refactor in mixed test

--- applicative-functors/src/test/kotlin/ApplicativeTest.kt
+++ applicative-functors/src/test/kotlin/ApplicativeTest.kt
@@ -170,20 +170,20 @@ class ApplicativeTest {
         val bothSubscribed = CountDownLatch(0) // Change this value to 2 to run the test slowly
         val subscribeThreadsStillRunning = CountDownLatch(0) // Change this value to 1 to run the test slowly
 
-        val service: (String, Int, String?, Int, String, String, String, String, String, String?, String) -> Single<String> =
+        val service: (String, String, String, String, String, String, String, String, String, String, String) -> Single<String> =
             { s1: String,
-              s2: Int,
-              s3: String?,
-              s4: Int,
+              s2: String,
+              s3: String,
+              s4: String,
               s5: String,
               s6: String,
               s7: String,
               s8: String,
               s9: String,
-              s10: String?,
+              s10: String,
               s11: String ->
                 val result =
-                    listOf(s1, "v$s2", s3 ?: "none", "v$s4", s5, s6, s7, s8, s9, s10 ?: "none", s11).joinToString(
+                    listOf(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11).joinToString(
                         separator = ";"
                     )
                 Single.just("Values:$result")
@@ -203,17 +203,17 @@ class ApplicativeTest {
 
         }
 
-        val s1: Single<String> = createSingle("v1")
-        val s2: Single<Int> = Single.just(2)
-        // Here, we move the Nullable value outside, so the whole Single<String> is Nullable, and not the value inside the Single`enter code here`
-        val s3: Single<String>? = null
-        val s4: Single<Int> = Single.just(4)
-        val s5: Single<String> = createSingle("v5")
-        val s6: Single<String> = createSingle("v6")
-        val s7: Single<String> = createSingle("v7")
-        val s8: Single<String> = createSingle("v8")
-        val s9: Single<String> = createSingle("v9")
-        val s10: Single<String>? = null
+        val s1 = createSingle("v1")
+        val s2 = createSingle("v2")
+        // Here, we move the Nullable value outside, so the whol is Nullable, and not the value inside the Single`enter code here`
+        val s3 = createSingle("v3")
+        val s4 = createSingle("v4")
+        val s5 = createSingle("v5")
+        val s6 = createSingle("v6")
+        val s7 = createSingle("v7")
+        val s8 = createSingle("v8")
+        val s9 = createSingle("v9")
+        val s10 = createSingle("v10")
         val s11 = createSingle("v11")
 
         // When
@@ -223,15 +223,15 @@ class ApplicativeTest {
 
         val result = singleFunction
             .zipOver(s1)
-            .zipOver(s2)
-            .zipOverNullable(s3)
-            .zipOver(s4)
-            .mapOver(s5)
+            .mapOver(s2)
+            .mapOver(s3)
+            .mapOver(s4)
+            .zipOver(s5)
             .zipOver(s6)
-            .mapOver(s7)
+            .zipOver(s7)
             .zipOver(s8)
             .mapOver(s9)
-            .zipOverNullable(s10)
+            .zipOver(s10)
             .mapOver(s11)
             .flatMap { it }
 
@@ -240,7 +240,7 @@ class ApplicativeTest {
             .test()
             .awaitDone(50, TimeUnit.SECONDS)
             .assertSubscribed()
-            .assertValues("Values:v1;v2;none;v4;v5;v6;v7;v8;v9;none;v11")
+            .assertValues("Values:v1;v2;v3;v4;v5;v6;v7;v8;v9;v10;v11")
     }
 
 }
